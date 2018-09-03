56afed34292c6ce0b7c1e98b2d623ad2493be110
amanullacshaik <z1727212@students.niu.edu>
2018-09-03 16:50:43 -0400

    sign up view

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { BookingComponent } from './modules/booking/components/booking/booking.component';
 import { LoginComponent } from './modules/booking/components/login/login.component';
+import { SignUpComponent } from './modules/booking/components/sign-up/sign-up.component';
 
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
@@ -7,7 +8,8 @@ import { ModuleWithProviders } from '@angular/core';
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'content' },
     { path: 'booking', component: BookingComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignUpComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
--- src/app/modules/booking/booking.module.ts
+++ src/app/modules/booking/booking.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingComponent } from './components/booking/booking.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BookingComponent, LoginComponent]
+  declarations: [BookingComponent, LoginComponent, SignUpComponent]
 })
 export class BookingModule { }
